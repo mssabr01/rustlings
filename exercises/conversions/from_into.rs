7b3dad6f1ae13da005d812c5463f3d6a871747b5
mehdi <mssabr01@louisville.edu>
2022-03-03 21:08:21 -0500

    conversions done

--- exercises/conversions/from_into.rs
+++ exercises/conversions/from_into.rs
@@ -33,10 +33,38 @@ impl Default for Person {
 // If while parsing the age, something goes wrong, then return the default of Person
 // Otherwise, then return an instantiated Person object with the results
 
-// I AM NOT DONE
 
 impl From<&str> for Person {
     fn from(s: &str) -> Person {
+        if s.len() == 0
+        {
+            return Person::default();
+        }
+        let mut split = s.split(",").collect::<Vec<&str>>();
+        
+        if split.len() != 2
+        {
+            return Person::default();
+        }
+
+        let name = split[0];
+        let age = split[1];
+
+        if name.len() == 0
+        {
+            return Person::default();
+        }
+        
+        if let Ok(x) = age.parse::<usize>()
+        {
+            return Person {
+                name: name.to_string(),
+                age: x,
+            };
+        }
+
+        return Person::default()
+
     }
 }
 
--- exercises/conversions/from_str.rs
+++ exercises/conversions/from_str.rs
@@ -26,7 +26,6 @@ enum ParsePersonError {
     ParseInt(ParseIntError),
 }
 
-// I AM NOT DONE
 
 // Steps:
 // 1. If the length of the provided string is 0, an error should be returned
@@ -41,6 +40,36 @@ enum ParsePersonError {
 impl FromStr for Person {
     type Err = ParsePersonError;
     fn from_str(s: &str) -> Result<Person, Self::Err> {
+        if s.len() == 0
+        {
+            return Err(ParsePersonError::Empty);
+        }
+        let mut split = s.split(",").collect::<Vec<&str>>();
+        
+        if split.len() != 2
+        {
+            return Err(ParsePersonError::BadLen);
+        }
+
+        let name = split[0];
+        let age = split[1];
+
+        if name.len() == 0
+        {
+            return Err(ParsePersonError::NoName);
+        }
+        
+
+        match age.parse::<usize>()
+        {
+            Ok(x) => return Ok(Person {
+                name: name.to_string(),
+                age: x,
+            }),
+            Err(x) => return Err(ParsePersonError::ParseInt(x)),
+        }
+        
+        
     }
 }
 
